bed1681a0d410857bd24420e6357501ff75707eb
LumberMan1301 <marianomm@rocketmail.com>
2017-11-10 03:10:33 -0600

    3.10

--- org.tec/datastructures/DatosLineales/Cola.java
+++ org.tec/datastructures/DatosLineales/Cola.java
@@ -1,71 +1,85 @@
 package datastructures.DatosLineales;
-public class Cola<T> {
-    private NodeLista<T> head = null;
-    private int capacidad = 0;
 
-    public Cola() {
+public class Cola<T>
+{
+    private NodeLista<T> head;
+    private int capacidad;
+
+    public Cola()
+    {
+        this.head = null;
+        this.capacidad = 0;
     }
 
-    public void Enqueve(T dato) {
+    public void Enqueve(T dato)
+    {
         NodeLista<T> nodo = new NodeLista(dato);
-        if(this.capacidad == 0) {
+        if (this.capacidad == 0)
+        {
             this.head = nodo;
-            this.head.setNext((NodeLista)null);
-            ++this.capacidad;
-        } else {
-            NodeLista current;
-            for(current = this.head; current.getNext() != null; current = current.getNext()) {
-                ;
+            this.head.setNext(null);
+            this.capacidad += 1;
+        }
+        else
+        {
+            NodeLista<T> current = this.head;
+            while (current.getNext() != null) {
+                current = current.getNext();
             }
-
             current.setNext(nodo);
-            current.getNext().setNext((NodeLista)null);
-            ++this.capacidad;
+            current.getNext().setNext(null);
+            this.capacidad += 1;
         }
-
     }
 
-    public T Peek() {
-        return this.head.getData();
+    public T Peek()
+    {
+        return (T)this.head.getData();
     }
 
-    public void Dequeve() {
-        if(this.capacidad != 0) {
-            if(this.capacidad == 1) {
+    public void Dequeve()
+    {
+        if (this.capacidad != 0) {
+            if (this.capacidad == 1)
+            {
                 this.head = null;
-                --this.capacidad;
-            } else {
+                this.capacidad -= 1;
+            }
+            else
+            {
                 this.head = this.head.getNext();
-                --this.capacidad;
+                this.capacidad -= 1;
             }
         }
-
     }
 
-    public int size() {
+    public int size()
+    {
         return this.capacidad;
     }
 
-    public String print() {
+    public String print()
+    {
         String fin = "[";
         NodeLista<T> aux = this.head;
-        if(aux == null) {
+        if (aux == null)
+        {
             fin = fin + "]";
             return fin;
-        } else {
-            while(aux.getNext() != null) {
-                if(aux.getNext() == null) {
-                    fin = fin + aux.getData().toString();
-                    aux = aux.getNext();
-                } else {
-                    fin = fin + aux.getData().toString() + ",";
-                    aux = aux.getNext();
-                }
-            }
-
-            fin = fin + aux.getData().toString() + "]";
-            return fin;
         }
+        while (aux.getNext() != null) {
+            if (aux.getNext() == null)
+            {
+                fin = fin + aux.getData().toString();
+                aux = aux.getNext();
+            }
+            else
+            {
+                fin = fin + aux.getData().toString() + ",";
+                aux = aux.getNext();
+            }
+        }
+        fin = fin + aux.getData().toString() + "]";
+        return fin;
     }
 }
-
--- org.tec/datastructures/DatosLineales/Grafo.java
+++ org.tec/datastructures/DatosLineales/Grafo.java
@@ -4,66 +4,61 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Iterator;
 
-public class Grafo<T> {
+public class Grafo<T>
+{
     private Hashtable<NodeGrafo<T>, ArrayList<NodeGrafo<T>>> adjList = new Hashtable();
     private ArrayList<NodeGrafo<T>> nodes = new ArrayList();
     private int numNodes = 0;
 
-    public Grafo() {
+    public void add(NodeGrafo<T> node)
+    {
+        if (this.adjList.contains(node)) {
+            return;
+        }
+        this.numNodes += 1;
+        this.adjList.put(node, new ArrayList());
+        this.nodes.add(node);
     }
 
-    public void add(NodeGrafo<T> node) {
-        if(!this.adjList.contains(node)) {
-            ++this.numNodes;
-            this.adjList.put(node, new ArrayList());
-            this.nodes.add(node);
+    public void addCamino(NodeGrafo<T> from, NodeGrafo<T> to)
+    {
+        if (!this.adjList.containsKey(from)) {
+            add(from);
         }
-    }
-
-    public void addCamino(NodeGrafo<T> from, NodeGrafo<T> to) {
-        if(!this.adjList.containsKey(from)) {
-            this.add(from);
+        if (!this.adjList.containsKey(to)) {
+            add(to);
         }
-
-        if(!this.adjList.containsKey(to)) {
-            this.add(to);
-        }
-
         ((ArrayList)this.adjList.get(from)).add(to);
-        ++to.inDegree;
+        to.inDegree += 1;
         to.inNodes.add(from);
     }
 
-    public void removeCamino(NodeGrafo<T> from, NodeGrafo<T> to) {
+    public void removeCamino(NodeGrafo<T> from, NodeGrafo<T> to)
+    {
         ((ArrayList)this.adjList.get(from)).remove(to);
-        --to.inDegree;
+        to.inDegree -= 1;
         to.inNodes.remove(from);
     }
 
-    public boolean checkCamino(NodeGrafo<T> from, NodeGrafo<T> to) {
+    public boolean checkCamino(NodeGrafo<T> from, NodeGrafo<T> to)
+    {
         return ((ArrayList)this.adjList.get(from)).contains(to);
     }
 
-    public void printGraph() {
-        Iterator var2 = this.nodes.iterator();
-
-        while(var2.hasNext()) {
-            NodeGrafo<T> node = (NodeGrafo)var2.next();
+    public void printGraph()
+    {
+        for (NodeGrafo<T> node : this.nodes)
+        {
             System.out.print("de: " + node.getData() + " |  a: ");
-            Iterator var4 = ((ArrayList)this.adjList.get(node)).iterator();
-
-            while(var4.hasNext()) {
-                NodeGrafo<T> m = (NodeGrafo)var4.next();
+            for (NodeGrafo<T> m : (ArrayList)this.adjList.get(node)) {
                 System.out.print(m.getData() + " ");
             }
-
             System.out.println();
         }
-
     }
 
-    public int getNumNodes() {
+    public int getNumNodes()
+    {
         return this.numNodes;
     }
 }
-
